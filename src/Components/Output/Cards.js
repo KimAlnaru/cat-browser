f4605587cbd57f42cb714f6267d74fc221df42f5
KimAlnaru <intanalnaru@gmail.com>
2022-11-21 21:02:18 +0800

    React route

--- src/Components/Output/Cards.js
+++ src/Components/Output/Cards.js
@@ -3,19 +3,19 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import { CatContext } from "../../Contexts/CatContext";
 import { useContext } from 'react';
-import {useHistory} from 'react-router-dom';
+// import {useHistory} from 'react-router-dom';
 
 function Cards({ id, url, breeds }) {
 
   const { setCatDetails, setCatBreed } = useContext(CatContext);
-  const history = useHistory();
+  // const history = useHistory();
 
   const saveCatDetails = (url, breeds) => {
     setCatDetails(url);
     setCatBreed(breeds);  
 
     
-    history.push(`/cat-browser/cat?q=${breeds}`);
+    // history.push(`/cat-browser/cat?q=${breeds}`);
     console.log(breeds);
   }
   
@@ -23,9 +23,9 @@ function Cards({ id, url, breeds }) {
     <Card key={id} className="mb-5">
       <Card.Img variant="top" src={url} alt={id}/>
       <Card.Body>
-        {/* <Link to='/cat-browser/cat'> */}
+        <Link to='/cat-browser/cat'>
           <Button className='w-100' variant="primary" onClick={() => saveCatDetails(url, breeds)}>View Details </Button>
-        {/* </Link>        */}
+        </Link>       
       </Card.Body>
     </Card>
   );
